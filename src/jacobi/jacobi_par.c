34f95842eca3b796012ead09e0e7615e52071797
Lucas Villarroel <vilucas7@gmail.com>
2021-03-12 15:18:21 +0100

    done jacobi parallel

--- src/jacobi/jacobi_par.c
+++ src/jacobi/jacobi_par.c
@@ -20,24 +20,47 @@
 int gridSize, numIters, numWorkers;
 
 void maxDiff(double** grid, double** new) {
+  int i, j;
   double maxDiff = 0.0;
-  for (int i = 1; i < gridSize; i++) {
-    for (int j = 1; j < gridSize; j++) {
-      maxDiff = (fabs(grid[i][j] - new[i][j]) > maxDiff) ? fabs(grid[i][j] - new[i][j]) : maxDiff;
+#pragma omp parallel for private(j)
+  for (i = 1; i < gridSize; i++) {
+    for (j = 1; j < gridSize; j++) {
+#pragma omp critical
+      {
+        maxDiff = (fabs(grid[i][j] - new[i][j]) > maxDiff) ? fabs(grid[i][j] - new[i][j]) : maxDiff;
+      }
     }
   }
   printf("maxDiff: %.20f \n", maxDiff);
 }
 
+void printGrid(double** grid) {
+  FILE* fp = fopen("par-data.out", "w");
+  for (int i = 0; i < gridSize; i++) {
+    fprintf(fp, "[");
+    for (int j = 0; j < gridSize; j++) {
+      if (j == 0) {
+        fprintf(fp, "%g", grid[i][j]);
+      } else {
+        fprintf(fp, ", %g", grid[i][j]);
+      }
+    }
+    fprintf(fp, "]\n");
+  }
+}
+
 void jacobi(double** grid, double** new) {
+  int i, j;
   for (int iter = 1; iter < numIters; iter += 2) {
-    for (int i = 1; i < gridSize - 1; i++) {
-      for (int j = 1; j < gridSize - 1; j++) {
+#pragma omp parallel for private(j)
+    for (i = 1; i < gridSize - 1; i++) {
+      for (j = 1; j < gridSize - 1; j++) {
         new[i][j] = (grid[i - 1][j] + grid[i + 1][j] + grid[i][j - 1] + grid[i][j + 1]) * 0.25;
       }
     }
-    for (int i = 1; i < gridSize - 1; i++) {
-      for (int j = 1; j < gridSize - 1; j++) {
+#pragma omp parallel for private(j)
+    for (i = 1; i < gridSize - 1; i++) {
+      for (j = 1; j < gridSize - 1; j++) {
         grid[i][j] = (new[i - 1][j] + new[i + 1][j] + new[i][j - 1] + new[i][j + 1]) * 0.25;
       }
     }
@@ -68,12 +91,17 @@ int main(int argc, char* argv[]) {
       if ((i == 0) || (j == 0) || (j == (gridSize - 1)) || (i == (gridSize - 1))) {
         grid[i][j] = 1;
         new[i][j] = 1;
-      }
-      else {
+      } else {
         grid[i][j] = 0;
         new[i][j] = 0;
       }
     }
   }
+
+  omp_set_num_threads(numWorkers);
+  jacobi(grid, new);
+  printGrid(grid);
+
+
   return 0;
 }
