d9155ae9743c4e1b7dfa2e8b3e1bbd8ea119b47c
ehanstad <erik_hanstad@hotmail.com>
2021-03-12 15:02:48 +0100

    ...

--- src/jacobi/jacobi_par.c
+++ src/jacobi/jacobi_par.c
@@ -10,6 +10,7 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <omp.h>
 
 #define MAX_ITERATIONS 100000000
@@ -17,6 +18,35 @@
 #define MAX_WORKERS 4
 
 int gridSize, numIters, numWorkers;
+
+void maxDiff(double** grid, double** new) {
+  double maxDiff = 0.0;
+  for (int i = 1; i < gridSize; i++) {
+    for (int j = 1; j < gridSize; j++) {
+      maxDiff = (fabs(grid[i][j] - new[i][j]) > maxDiff) ? fabs(grid[i][j] - new[i][j]) : maxDiff;
+    }
+  }
+  printf("maxDiff: %.20f \n", maxDiff);
+}
+
+void jacobi(double** grid, double** new) {
+  for (int iter = 1; iter < numIters; iter += 2) {
+    for (int i = 1; i < gridSize - 1; i++) {
+      for (int j = 1; j < gridSize - 1; j++) {
+        new[i][j] = (grid[i - 1][j] + grid[i + 1][j] + grid[i][j - 1] + grid[i][j + 1]) * 0.25;
+      }
+    }
+    for (int i = 1; i < gridSize - 1; i++) {
+      for (int j = 1; j < gridSize - 1; j++) {
+        grid[i][j] = (new[i - 1][j] + new[i + 1][j] + new[i][j - 1] + new[i][j + 1]) * 0.25;
+      }
+    }
+#ifdef debug
+    printf("iter %d/%d\n", iter, numIters);
+#endif // debug
+  }
+  maxDiff(grid, new);
+}
 
 int main(int argc, char* argv[]) {
   // take command line args
